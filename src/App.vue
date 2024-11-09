c4ad3935e9c425c017af2bd5a603099584486ba6
vasicgoran1984 <vasicgoran.1984@gmail.com>
2024-11-09 15:01:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <ul>
+        <learning-resource v-for="res in storedResources" :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        >
+        </learning-resource>
+    </ul>
+</template>
+
+<script>
+
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+    components: {
+        LearningResource,
+    },
+    data() {
+        return {
+            // Array
+            storedResources: [
+                // Objects
+                {
+                    id: 'official-guide',
+                    title: 'Official Gudie',
+                    description: 'The oficial Vue.js documentation',
+                    link: 'https://vue.org',
+                },
+                {
+                    id: 'Google',
+                    title: 'Official Google site',
+                    description: 'The oficial google documentation',
+                    link: 'https://google.com',
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style>
+@import url("http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'");
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
